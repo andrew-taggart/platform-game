0b59fc344426822a8baf85ffb39d55ed046ac792
andrew-taggart <andrew.taggart53@gmail.com>
2024-01-28 19:59:19 -0500

    Gravity and Collision

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let currentKey
-let y = 100
-let player = document.querySelector("#player")
+let floor = document.querySelector(".floor")
+let player = document.querySelector(".player")
+let container = document.querySelector(".container")
+let up = document.querySelector(".up")
+let down = document.querySelector(".down")
+let left = document.querySelector(".left")
+let right = document.querySelector(".right")
 
+//Functions
 
+let gravitySpd = setInterval(gravity, 1000/50);
 
-function animate() {
-    window.requestAnimationFrame(animate)
-    console.log('animate')
-    player.style.background = "red"
-    
+function gravity() {
+    if (player.offsetTop <= (floor.offsetTop - 55)) {
+        player.style.top = player.offsetTop + 1 + "px"
+    } else if (player.offsetTop == floor.offsetTop + 200)
+    player.style.top =  200 + "px"
 }
-
-animate()
