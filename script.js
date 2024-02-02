df90e88a7b867a9a49ab90b33533cf98879f8d62
andrew-taggart <andrew.taggart53@gmail.com>
2024-02-02 07:35:52 -0500

    center & contain games style

--- script.js
+++ script.js
@@ -3,36 +3,33 @@ const objects = document.querySelectorAll(".game-object")
 const lava = document.querySelectorAll('.lava')
 const goal = document.querySelector('.goal')
 const restartBtn = document.querySelector(".playAgain")
-
-//Check Locations
+const endMsg = document.querySelector('.popup')
 
 //Game Values
 const gravity = 1
 const keys = {}
 //Player
-let pX = 50
-let pY = 50
+let pX = 1
+let pY = 1
 let velocityX = 0
 let velocityY = 0
 let jumping = false
 
 //Collision Equation
 function collision(obj1, obj2) {
-    return obj1.x < obj2.x + obj2.width &&
+    return (obj1.x < obj2.x + obj2.width &&
            obj1.x + obj1.width > obj2.x &&
            obj1.y < obj2.y + obj2.height &&
-           obj1.y + obj1.height > obj2.y
+           obj1.y + obj1.height > obj2.y)
 }
 
 function checkGoal() {
     const goalLocation = goal.getBoundingClientRect()
     const playerLocation = player.getBoundingClientRect()
     if (collision(playerLocation, goalLocation)) {
-        alert('Congrats')
+        endMsg.style.display = 'block'
     }
 }
-
-
 
 //Event Listeners
 restartBtn.addEventListener('click', restartGame)
@@ -64,12 +61,15 @@ function gameLoop() {
     player.style.left = pX + 'px'
     player.style.top = pY + 'px'
 
+    player.style.top = `${pY}px`
+    player.style.left = `$${pX}px`
+
     //Collision Implementation
     objects.forEach((object) => {
         if (object !== player) {
         const objLocation = object.getBoundingClientRect()
         const playerLocation = player.getBoundingClientRect()
-       
+       //Adjust collision implementation, but keep current logic for sticky walls
 
         if (collision(playerLocation, objLocation)) {
             pY -= velocityY
